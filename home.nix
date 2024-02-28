151445457723caf1cdd76f5083b8e589c9155126
= <=>
2024-02-28 20:36:16 +0100

    Enable ozone for electron apps

--- home.nix
+++ home.nix
@@ -6,11 +6,14 @@
   # Let home-manager install and manage itself
   programs.home-manager.enable = true;
 
+  systemd.user.sessionVariables = {
+    NIXOS_OZONE_WL = 1;
+  };
+
   home = {
     stateVersion = "23.11";
     username = "naveen";
     homeDirectory = "/home/naveen";
-
-    packages = with pkgs; [thunderbird];
+    packages = with pkgs; [obsidian thunderbird spotify];
   };
 }
